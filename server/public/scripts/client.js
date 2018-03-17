e863104bff31d35b0112a3836a0799b0b93b507a
Kam Kubesh <kube0057@umn.edu>
2018-03-17 11:02:30 -0500

    adjustments to html, switch statement to server.js, and removed caps on math for to prevent syntax issues.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,28 +15,50 @@ function clickHandler() {
 function subAdd (){
     let x = $('#xVal').val();
     let y = $('#yVal').val();
-    let valsAdd = {xValue: x, yValue: y, Math: "Add"};
+    let valsAdd = {xValue: x, yValue: y, math: "Add"};
     console.log(valsAdd);
-    
+    $.ajax({
+        type: 'post',
+        data: valsAdd,
+        url: '/calc'
+    })
 }
 
 function subSubtract (){
     let x = $('#xVal').val();
     let y = $('#yVal').val();
-    let valsAdd = {xValue: x, yValue: y, Math: "Subtract"};
+    let valsAdd = {xValue: x, yValue: y, math: "Subtract"};
     console.log(valsAdd);
+    $.ajax({
+        type: 'post',
+        data: valsAdd,
+        url: '/calc'
+    })
 }
 
 function subMultiply (){
     let x = $('#xVal').val();
     let y = $('#yVal').val();
-    let valsAdd = {xValue: x, yValue: y, Math: "Multiply"};
+    let valsAdd = {xValue: x, yValue: y, math: "Multiply"};
     console.log(valsAdd);
+    $.ajax({
+        type: 'post',
+        data: valsAdd,
+        url: '/calc'
+    })
 }
 
 function subDivide (){
     let x = $('#xVal').val();
     let y = $('#yVal').val();
-    let valsAdd = {xValue: x, yValue: y, Math: "Divide"};
+    let valsAdd = {xValue: x, yValue: y, math: "Divide"};
     console.log(valsAdd);
+    $.ajax({
+        type: 'post',
+        data: valsAdd,
+        url: '/calc'
+    }).done(function(response){
+        console.log('Success!');
+        
+    })
 }
--- server/server.js
+++ server/server.js
@@ -11,3 +11,30 @@ app.listen(PORT, () => {
     console.log('Server is running on', PORT);
 });
 
+app.post('/calc', (req,res) => {
+    let valsCalc = req.body;
+    console.log(valsCalc);
+    let operator = valsCalc.math;
+    let x = valsCalc.xValue;
+    let y = valsCalc.yValue;
+    console.log(operator, x, y);
+    switch(operator){
+        case 'Add':
+        let addTotal = parseInt(x) + parseInt(y)
+        console.log(addTotal);
+        break;
+        case 'Subtract':
+        let subTotal = parseInt(x) - parseInt(y) // parseInt not needed...
+        console.log(subTotal);
+        break;
+        case 'Multiply':
+        let multTotal = parseInt(x) * parseInt(y) // parseInt not needed...
+        console.log(multTotal);
+        break;
+        case 'Divide':
+        let divTotal = parseInt(x) + parseInt(y) // parseInt not needed...
+        console.log(divTotal);
+        break;
+    }
+    res.sendStatus(200);
+})
